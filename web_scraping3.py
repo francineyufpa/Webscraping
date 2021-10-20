bd148ee896d951956f681dc822b2fbeb1e8f42b1
FRANCINEY PALHETA <franciney@ufpa.br>
2021-10-19 22:21:49 -0300

    Update web_scraping3.py

--- web_scraping3.py
+++ web_scraping3.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 # Vamos mudar a URL
 #url = 'https://www.python.org/'
-url = https://finance.yahoo.com/quote/VALE3.SA/history?period1=946699200&period2=1634615999&interval=1d&frequency=1d&filter=history
+url = 'https://finance.yahoo.com/quote/VALE3.SA/history?period1=946699200&period2=1634615999&interval=1d&frequency=1d&filter=history'
 # lendo a URL com a urllopen
 #html = urlopen(url)
 html = requests.get(url).content
