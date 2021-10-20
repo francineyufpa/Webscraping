3a51d83fa5d27ce8de9243b68d72c566b3e9de25
FRANCINEY PALHETA <franciney@ufpa.br>
2021-10-20 09:40:04 -0300

    Update web_scraping4.py

--- web_scraping4.py
+++ web_scraping4.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup
 
 # Vamos mudar a URL
 #url = 'https://www.python.org/'
-url = 'https://finance.yahoo.com/quote/VALE3.SA/history?period1=946699200&period2=1634615999&interval=1d&frequency=1d&filter=history'
+#url = 'https://finance.yahoo.com/quote/VALE3.SA/history?period1=946699200&period2=1634615999&interval=1d&frequency=1d&filter=history'
+
+url = 'https://ge.globo.com/futebol/brasileirao-serie-b/'
 
 # lendo a URL com a urllopen
 #html = urlopen(url)
@@ -16,8 +18,25 @@ html = requests.get(url).text
 # Imprimindo o título da página
 soup = BeautifulSoup(html, 'html.parser')
 #print(soup.prettify())
-for tabela in soup.findAll('h1'):
-    print(tabela)
-
-#print(soup.findAll('table', class_='W(100%) M(0)'))
-
+#for tabela in soup.findAll('div'):
+#    print(tabela)
+#print(len(soup.findAll('div')))
+#print(soup.findAll('div', class_='classificacao__pontos-corridos'))
+#print(soup.findAll('div', class_="classificacao__pontos-corridos"))
+#soup.find( class_ ="classificacao__pontos-corridos")
+# iterate all tags
+# get all tags
+tags = {tag.name for tag in soup.find_all()}
+class_list = set()
+for tag in tags:
+  
+    # find all element of tag
+    for i in soup.find_all( tag ):
+  
+        # if tag has attribute of class
+        if i.has_attr( "class" ):
+  
+            if len( i['class'] ) != 0:
+                class_list.add(" ".join( i['class']))
+  
+print(class_list)
